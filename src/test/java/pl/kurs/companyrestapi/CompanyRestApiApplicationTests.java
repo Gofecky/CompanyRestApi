824e3b8d83f98ee02ada582d2be5a19fe949f48a
Patryk <asdasd@op.pl>
2025-03-15 17:18:28 +0100

    init commit

---
+++ src/test/java/pl/kurs/companyrestapi/CompanyRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kurs.companyrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
